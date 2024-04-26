7c551a3cdc6cec4491792a0daeb4e7deba396a0b
TeeGreat <toyinatofarati64@gmail.com>
2024-04-26 08:37:42 +0100

    Home Screen

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+// import icons from "./icons";
+// import images from "./images";
+
+// export { icons, images };
+
+import icons from "./icons.js";
+import images from "./images.js";
+
+
+export { icons, images };
